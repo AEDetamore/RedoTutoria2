f7a0a4e29345b8f3b4e02e550826864be3d6cb70
AEDetamore <77295532+AEDetamore@users.noreply.github.com>
2021-03-14 08:29:42 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,22 +6,40 @@ using UnityEngine.UI;
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
-
+    
     public float speed;
 
-    public Text score;
+    public Text scoreText;
+    public Text winText;
+    public Text livesText;
+    public Text loseText;
 
     private int scoreValue = 0;
+    private int livesValue = 3;
+
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
     
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        scoreText.text = "Score: " + scoreValue.ToString();
+        livesText.text = "Lives: " + livesValue.ToString();
+
+        musicSource.clip = musicClipOne;
+        musicSource.loop = true;
+
+        SetScoreValue();
+        SetLivesText();
+
+        winText.text = "";
+        loseText.text = "";
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float vertMovement = Input.GetAxis("Vertical");
@@ -33,11 +51,28 @@ public class PlayerScript : MonoBehaviour
      if(collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            SetScoreValue();
+            scoreText.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector2(43.0f, 3.0f);
+                livesValue = 3;
+                livesText.text = "Lives: " + livesValue.ToString();
+                scoreValue = 4;
+                scoreText.text = "Score: " + scoreValue.ToString();
+            }
+        }
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            SetLivesText();
+            livesText.text = livesValue.ToString();
             Destroy(collision.collider.gameObject);
         }
-
     }
+
+
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.collider.tag == "Ground")
@@ -46,6 +81,27 @@ public class PlayerScript : MonoBehaviour
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
+        }
+    }
+
+    void SetScoreValue()
+    {
+        scoreText.text = "Score: " + scoreValue.ToString();
+        if (scoreValue >= 8)
+        {
+            winText.text = "You Win! Game create by Abigail Detamore";
+            musicSource.loop = false;
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+
+        }
+    }
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + livesValue.ToString();
+        if (livesValue == 0)
+        {
+            loseText.text = "You Lose! Game created by Abigail Detamore";
         }
     }
 }
